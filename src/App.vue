1e3ce288c29dad38e88a056b78d2dd777118a8ca
yangliulang <yang_liulang@126.com>
2020-07-03 23:44:28 +0800

    ref

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
   <div>
     <h1>Vue 3.0</h1>
-    <HelloWord
-      :info="{ name: 'yangyong', age: 30 }"
-      @cao="() => {}"
-      id="app"
-      class="apps"
-      ref="app"
-    >
-      <p>solts p</p>
+    {{ state.isDestory }}
+    <HelloWord @setDistory="setDistory" v-if="!state.isDestory">
+      <p>solts p1</p>
+      <template v-slot:p2="{ name, age }">
+        <p>solts p2 {{ name }}{{ age }}</p>
+      </template>
     </HelloWord>
   </div>
 </template>
 <script>
 import HelloWord from '@/components/HelloWorld'
+import { reactive } from 'vue'
 export default {
   components: { HelloWord },
+  setup() {
+    const state = reactive({
+      isDestory: false,
+    })
+    const setDistory = () => {
+      // console.log('setDistory......')
+      state.isDestory = !state.isDestory
+    }
+    return { state, setDistory }
+  },
 }
 </script>
